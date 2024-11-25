1b2e34e7071175108ffa63c08d6cac2d68d3216f
Christian Santanas <50605403+chris171290@users.noreply.github.com>
2024-11-25 17:28:10 -0500

    feat(api): :sparkles: add payments module

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { UsersModule } from './users/users.module';
-import { NatsClientModule } from './nats-client/nats-client.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
